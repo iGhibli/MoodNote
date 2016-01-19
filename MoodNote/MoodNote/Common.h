d787e8d56eeadc79221047124a54be143d242c12
iGhibli <iGhibli@163.com>
2016-01-19 19:33:21 +0800

    完成TableView横向滑动设置

---
+++ MoodNote/MoodNote/Common.h
@@ -0,0 +1,15 @@
+//
+//  Common.h
+//  MoodNote
+//
+//  Created by qingyun on 16/1/19.
+//  Copyright © 2016年 qingyun. All rights reserved.
+//
+
+#ifndef Common_h
+#define Common_h
+
+#define ScreenW [UIScreen mainScreen].bounds.size.width
+#define ScreenH [UIScreen mainScreen].bounds.size.height
+
+#endif /* Common_h */
--- MoodNote/MoodNote/ViewController.m
+++ MoodNote/MoodNote/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "Common.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
@@ -16,7 +18,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self CreateAndSetUpSubViews];
+}
+
+- (void)CreateAndSetUpSubViews
+{
+    [self.view addSubview:self.tableView];
+}
+
+- (UITableView *)tableView {
+    if (_tableView == nil) {
+        _tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, ScreenH, ScreenW) style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.transform = CGAffineTransformMakeRotation(-M_PI_2);
+        _tableView.center = CGPointMake(ScreenW / 2, ScreenH / 2);
+        _tableView.showsVerticalScrollIndicator = NO;
+        _tableView.pagingEnabled = YES;
+        _tableView.backgroundColor = [UIColor redColor];
+    }
+    return _tableView;
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 7;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return ScreenW;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CellID"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"CellID"];
+    }
+    cell.transform = CGAffineTransformMakeRotation(M_PI_2);
+    cell.textLabel.text = @"TEST";
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
