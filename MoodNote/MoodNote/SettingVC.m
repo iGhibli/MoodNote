e25b5759e84c22e59dc9fb252a211a4028cb2288
iGhibli <iGhibli@163.com>
2016-01-21 16:47:06 +0800

    添加关于和清理缓存

--- MoodNote/MoodNote/SettingVC.m
+++ MoodNote/MoodNote/SettingVC.m
@@ -7,9 +7,11 @@
 //
 
 #import "SettingVC.h"
+#import "Common.h"
 
 @interface SettingVC ()
-
+@property (nonatomic, strong) UIView *aboutView;
+@property (nonatomic, strong) UIView *clearView;
 @end
 
 @implementation SettingVC
@@ -17,6 +19,76 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self addSwipeGesture];
+}
+
+#pragma mark - Getter & Setter
+- (UIView *)aboutView {
+    if (_aboutView == nil) {
+        _aboutView = [[UIView alloc]initWithFrame:[UIScreen mainScreen].bounds];
+        _aboutView.backgroundColor = [UIColor grayColor];
+        _aboutView.alpha = 0.98;
+//        [self.view addSubview:_aboutView];
+        
+        UILabel *author = [[UILabel alloc]initWithFrame:CGRectMake(10, kScreenH / 2 - 35, kScreenW - 20, 30)];
+        author.text = @"Author: ZhouShuaijie";
+        author.font = [UIFont systemFontOfSize:22];
+        author.textAlignment = NSTextAlignmentCenter;
+        [_aboutView addSubview:author];
+        UILabel *email = [[UILabel alloc]initWithFrame:CGRectMake(10, kScreenH / 2, kScreenW - 20, 30)];
+        email.text = @"Email: iGhibli@163.com";
+        email.textAlignment = NSTextAlignmentCenter;
+        [_aboutView addSubview:email];
+        UILabel *version = [[UILabel alloc]initWithFrame:CGRectMake(10, kScreenH / 2 + 35, kScreenW - 20, 30)];
+        version.text = @"Version: 1.0";
+        version.font = [UIFont systemFontOfSize:16];
+        version.textAlignment = NSTextAlignmentCenter;
+        [_aboutView addSubview:version];
+        UILabel *thanks = [[UILabel alloc]initWithFrame:CGRectMake(10, kScreenH / 2 + 70, kScreenW - 20, 30)];
+        thanks.text = @"^_^ 谢谢使用 ^_^";
+        
+        thanks.textAlignment = NSTextAlignmentCenter;
+        [_aboutView addSubview:thanks];
+        
+        UIButton *cancel = [[UIButton alloc]init];
+        cancel.center = CGPointMake(kScreenW / 2, kScreenH - 30);
+        cancel.bounds = CGRectMake(0, 0, 40, 40);
+        [cancel setImage:[UIImage imageNamed:@"btn-cancel-1"] forState:UIControlStateNormal];
+        [cancel setImage:[UIImage imageNamed:@"btn-cancel-2"] forState:UIControlStateHighlighted];
+        [cancel addTarget:self action:@selector(hiddenView) forControlEvents:UIControlEventTouchUpInside];
+        [_aboutView addSubview:cancel];
+    }
+    return _aboutView;
+}
+
+- (UIView *)clearView {
+    if (_clearView == nil) {
+        _clearView = [[UIView alloc]initWithFrame:[UIScreen mainScreen].bounds];
+        _clearView.backgroundColor = [UIColor grayColor];
+        _clearView.alpha = 0.98;
+        
+        UILabel *memory = [[UILabel alloc]initWithFrame:CGRectMake(10, kScreenH / 2 - 35, kScreenW - 20, 30)];
+        memory.text = @"当前缓存: 1024 Kb";
+        memory.font = [UIFont systemFontOfSize:22];
+        memory.textAlignment = NSTextAlignmentCenter;
+        [_clearView addSubview:memory];
+        
+        UIButton *cancel = [[UIButton alloc]init];
+        cancel.center = CGPointMake(kScreenW / 2 - 30, kScreenH - 30);
+        cancel.bounds = CGRectMake(0, 0, 40, 40);
+        [cancel setImage:[UIImage imageNamed:@"btn-cancel-1"] forState:UIControlStateNormal];
+        [cancel setImage:[UIImage imageNamed:@"btn-cancel-2"] forState:UIControlStateHighlighted];
+        [cancel addTarget:self action:@selector(hiddenView) forControlEvents:UIControlEventTouchUpInside];
+        [_clearView addSubview:cancel];
+        UIButton *clearBtn = [[UIButton alloc]init];
+        clearBtn.center = CGPointMake(kScreenW / 2 + 30, kScreenH - 30);
+        clearBtn.bounds = CGRectMake(0, 0, 50, 50);
+        [clearBtn setImage:[UIImage imageNamed:@"btn-huancun-1"] forState:UIControlStateNormal];
+        [clearBtn setImage:[UIImage imageNamed:@"btn-huancun-2"] forState:UIControlStateHighlighted];
+        [clearBtn addTarget:self action:@selector(hiddenView) forControlEvents:UIControlEventTouchUpInside];
+        [_clearView addSubview:clearBtn];
+        
+    }
+    return _clearView;
 }
 
 - (void)addSwipeGesture
@@ -27,4 +99,22 @@
     [self.view addGestureRecognizer:swipeDown];
 }
 
+#pragma mark - ButtonAction
+- (IBAction)clearAction:(UIButton *)sender {
+    [self.view addSubview:self.clearView];
+}
+
+- (IBAction)goodAction:(UIButton *)sender {
+    
+}
+
+- (IBAction)aboutAction:(UIButton *)sender {
+    [self.view addSubview:self.aboutView];
+}
+
+- (void)hiddenView
+{
+    [self.aboutView removeFromSuperview];
+    [self.clearView removeFromSuperview];
+}
 @end
